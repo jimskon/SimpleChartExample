89a23bc54ff3be9b72027e04de345fccab448565
James Skon <skonjp@kenyon.edu>
2023-02-28 10:46:28 -0500

    First

---
+++ mychart.js
@@ -0,0 +1,39 @@
+const labels = ["Jan","Feb","Mar","Apr","May","June","July"]
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    fill: false,
+    borderColor: 'rgb(75, 192, 192)',
+    tension: 0.1
+  }]
+};
+ 
+const config = {
+  type: 'line',
+  data: data,
+};
+ 
+ const ctx = document.getElementById('myChart');
+
+  /*new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });*/
+  
+new Chart(ctx, config);
