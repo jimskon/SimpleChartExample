90a2e7ca6ea7abfba6ef8dd4834fe2931f44706f
jimskon <jamesskon@gmail.com>
2023-02-28 10:55:24 -0500

    Update mychart.js

--- mychart.js
+++ mychart.js
@@ -1,9 +1,10 @@
 const labels = ["Jan","Feb","Mar","Apr","May","June","July"]
+const stats = [65, 59, 80, 81, 56, 55, 40];
 const data = {
   labels: labels,
   datasets: [{
     label: 'My First Dataset',
-    data: [65, 59, 80, 81, 56, 55, 40],
+    data: stats,
     fill: false,
     borderColor: 'rgb(75, 192, 192)',
     tension: 0.1
