ec713de312e8bd5a2851d5c0cbf50c73274f68b1
jimskon <jamesskon@gmail.com>
2023-02-28 11:09:02 -0500

    Update mychart.js

--- mychart.js
+++ mychart.js
@@ -1,10 +1,17 @@
 const labels = ["Jan","Feb","Mar","Apr","May","June","July"]
-const stats = [65, 59, 80, 81, 56, 55, 40];
+const site1 = [65, 59, 80, 81, 56, 55, 40];
+const site2 = [24, 34, 54, 62, 34, 54, 23];
 const data = {
   labels: labels,
   datasets: [{
     label: 'Kilowatt hours by month',
-    data: stats,
+    data: site1,
+    fill: false,
+    borderColor: 'rgb(75, 192, 192)',
+    tension: 0.1
+  },{
+    label: 'Kilowatt hours by month',
+    data: site2,
     fill: false,
     borderColor: 'rgb(75, 192, 192)',
     tension: 0.1
